bcd0d625c4960338e06b4d8f14668876f4bdc5dc
Hadizha Kozubaeva <hadizhakozubaeva@gmail.com>
2024-04-24 02:02:49 +0600

    filter by year functionality was added

--- expenses-app/src/App.jsx
+++ expenses-app/src/App.jsx
@@ -1,30 +1,53 @@
+import { useState } from "react";
 import { Expenses } from "./components/expenses/Expenses";
-import { NewExpense } from "./components/new-expense/NewExpense";
-
-const expenses = [
-  {
-    // Страхование автомобилей
-    title: "Car Insurance",
-    price: 300,
-    date: new Date(2021, 2, 28),
-    id: "f1",
-  },
-  {
-    // Медицинское страхование
-    title: "Health Insurance",
-    price: 400,
-    date: new Date(2021, 2, 29),
-    id: "f2",
-  },
-];
+import NewExpense from "./components/new-expense/NewExpense";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    {
+      id: 1,
+      title: "Шаурма",
+      price: 94,
+      date: new Date(),
+    },
+    {
+      id: 2,
+      title: "Ноутбук",
+      price: 799,
+      date: new Date(),
+    },
+    {
+      id: 3,
+      title: "Iphone 12 Pro Max",
+      price: 294,
+      date: new Date(),
+    },
+    {
+      id: 4,
+      title: "Ужин с женой",
+      price: 450,
+      date: new Date(),
+    },
+  ]);
+
+  function addExpenseHandler(data) {
+    data.id = Math.floor(Math.random() * 1000);
+
+    setExpenses((prevState) => {
+      return [...prevState, data];
+    });
+  }
+  function deleteExpenseHandler(id) {
+    const exchangeExpenses = expenses.filter((item) => item.id !== id);
+    setExpenses(exchangeExpenses);
+  }
+
   return (
     <div className="wrapper">
       <div className="container">
-        <NewExpense />
-        <Expenses expenses={expenses} />
-      </div>
+        <NewExpense onAdd={addExpenseHandler} />
+        <Expenses data={expenses} onDelete={deleteExpenseHandler} />
+          </div>
     </div>
   );
 }
--- expenses-app/src/components/expenses/Expenses.jsx
+++ expenses-app/src/components/expenses/Expenses.jsx
@@ -1,18 +1,36 @@
-// more expenses components will be added here
-import { ExpenseItem } from "../expense-item/ExpenseItem";
 import PropTypes from "prop-types";
-import css from "./Expenses.module.css";
+import { ExpenseItem } from "../expense-item/ExpenseItem";
+import { ExpenseFilter } from "../expense-filter/ExpenseFilter";
+import { useState } from "react";
 
-export const Expenses = ({ expenses }) => {
+export const Expenses = ({ data, onDelete }) => {
+  const [selectedYear, setSelectedYear] = useState("All");
+
+  const yearChangeHandler = (event) => {
+    setSelectedYear(event.target.value);
+  };
+
+  const filteredExpenses = data.filter((item) => {
+    if (selectedYear) {
+      return item.date.getFullYear().toString() === selectedYear;
+    }
+    return false;
+  });
+
+  const renderedExpenses = selectedYear === "All" ? data : filteredExpenses;
+
   return (
-    <ul className={css.expenseWrapper}>
-      {expenses.map((item) => {
+    <ul>
+      <ExpenseFilter value={selectedYear} onChange={yearChangeHandler} />
+      {renderedExpenses.map((item) => {
         return (
           <ExpenseItem
             key={item.id}
-            title={item.title}
             date={item.date}
+            title={item.title}
             price={item.price}
+            id={item.id}
+            onDelete={onDelete}
           />
         );
       })}
@@ -21,5 +39,6 @@ export const Expenses = ({ expenses }) => {
 };
 
 Expenses.propTypes = {
-  expenses: PropTypes.array.isRequired,
+  data: PropTypes.array.isRequired,
+  onDelete: PropTypes.func,
 };
